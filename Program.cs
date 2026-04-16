7a3e43bb1eb14f21378ac1546acb4e073f0bd3bd
user <user@WIN-HSBA9HVO2EN>
2026-04-16 11:56:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите двузначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        int firstnumber = number / 10;
+        int secondnumber = number % 10;
+        Console.WriteLine($"Первая цифра: {firstnumber}");
+        Console.WriteLine($"Вторая цифра: {secondnumber}");
+    }
+}
